d6c573e1ce4b537ff10b05715e17e6645ca20e47
Amina Magomedova <am13870@nyu.edu>
2024-09-29 20:58:33 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let sharpShape=document.getElementById("sharpShape")
+
+sharpShape.addEventListener("click", () => {
+    document.getElementById("textbox").innerText = "You are a superstar!"
+})
+
+let mildShape=document.getElementById("mildShape")
+
+mildShape.addEventListener("click", () => {
+    document.getElementById("textbox").innerText = "You are an ultrastar!"
+})
+
+let dullShape=document.getElementById("dullShape")
+
+dullShape.addEventListener("click", () => {
+    document.getElementById("textbox").innerText = "You are a megastar!"
+})
